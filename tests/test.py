74f4c0a9a35989f86627f710473c22f228180ec2
ChrisM <cmakrygi@gmail.com>
2015-11-22 20:39:14 +0200

    just some testing

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import unittest
+from span_super_league.soccer_league import GameResult, RankingTable
+
+class TestGameResult(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
